f084f46dc1eb9b67815dc25f5481f14ff9d5400c
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-10-24 14:53:53 +0200

    Added homepage elements

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./home.css";
 export const Home = () => {
+  const specialWord = "<web>";
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="home">
+        <p>Hi! I'm Mohamed Mankouchi</p>
+        <h1>
+          Passionated <br />
+          Full-Stack developer <br />
+          that makes things for the <span>{specialWord}</span>
+        </h1>
+      </div>
+    </>
+  );
+};
