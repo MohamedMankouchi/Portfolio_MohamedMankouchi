8204aff993f5ce20822eb6a153c35cafb67cc662
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-10-25 14:20:08 +0200

    Skills

---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Skills = () => {
+  return (
+    <>
+      <div className="skills">
+        <div className="skills-title">
+          <h3>ABOUT ME</h3>
+        </div>
+      </div>
+    </>
+  );
+};
