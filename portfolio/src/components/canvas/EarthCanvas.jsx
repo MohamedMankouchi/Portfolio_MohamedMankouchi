324d3ff9c93bb1eae2b517394cb12d0d45769702
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-10-30 21:56:20 +0100

    Enabling scroll on canvas

--- portfolio/src/components/canvas/EarthCanvas.jsx
+++ portfolio/src/components/canvas/EarthCanvas.jsx
@@ -13,31 +13,33 @@ const Earth = () => {
 
 export const EarthCanvas = () => {
   return (
-    <Canvas
-      style={{ height: "250px", touchAction: "auto !important" }}
-      shadows
-      frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          enableRotate={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
+    <div className="earthCanvas">
+      <Canvas
+        style={{ height: "250px", touchAction: "auto" }}
+        shadows
+        frameloop="demand"
+        dpr={[1, 2]}
+        gl={{ preserveDrawingBuffer: true }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 6],
+        }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            autoRotate
+            enableZoom={false}
+            enableRotate={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Earth />
 
-        <Preload all />
-      </Suspense>
-    </Canvas>
+          <Preload all />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 };
