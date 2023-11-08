3e14e38dba8cba25b2a6f46b6d65d2f8bd0ea76d
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-11-08 10:22:45 +0100

    Animation on homepage

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import "./home.css";
 import Me from "./../assets/me.jpeg";
 import Me2 from "./../assets/me2.jpg";
@@ -6,9 +6,38 @@ import "./../App.css";
 import Dots from "./../assets/side-dots.png";
 import { gsap } from "gsap";
 import Typewriter from "typewriter-effect";
-
 export const Home = () => {
   const ref = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.fromTo(
+        ".me",
+        2,
+        {
+          opacity: 0,
+          ease: "expo.out",
+        },
+        {
+          opacity: 0.7,
+        }
+      );
+      gsap.fromTo(
+        ".txt",
+        1,
+        {
+          x: "-50%",
+          ease: "power4.inOut",
+        },
+        {
+          x: 0,
+        }
+      );
+    }, ref);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
       <div className="home" id="home" ref={ref}>
@@ -23,7 +52,7 @@ export const Home = () => {
           <div className="dotDiv">
             <img className="dots" src={Dots} />
           </div>
-          <h1>
+          <h1 className="txt">
             Passionated <br />
             Full-Stack developer <br />
             that makes things for{" "}
